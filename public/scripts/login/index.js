ee439a6b245eeb66251e30a11bedbf279de87335
Kyle1668 <kyledevinobrien1@gmail.com>
2017-07-22 22:42:07 -0700

    fixed jquery

--- public/scripts/login/index.js
+++ public/scripts/login/index.js
@@ -1,29 +1,29 @@
-/**
- * Created by kyleobrien on 7/22/17.
- */
 
-var ERROR_MESSAGE = "<strong class=\"error\">Invalid Input: Username or password is incorrect.</strong>"
+var ERROR_MESSAGE = "<strong class=\"error\">Incorrect Username or Password.</strong>";
+
+function clearErrorMessage() {
+    var errorMessage = $(".error");
+    if (errorMessage.count !== 0) {
+        errorMessage.fadeOut("fast", "linear", function() {
+            this.remove();
+        });
+    }
+}
 
 function submit() {
     var nameInput = $("#UserNameInput").val();
+    var validUsernameInput = nameInput !== "";
     var passwordInput = $("#PasswordInput").val();
-    var errorMessagePrinted = $(".error").length === 0
-    var validInput = nameInput !== "" || passwordInput !== "";
-
-    console.log(validInput);
-    console.log(nameInput);
-    console.log(passwordInput);
-
-    if (validInput) {
-        /*location.href = 'app.html';*/
-    }
-    else if (errorMessagePrinted) {
-        $("#SubmitButton").after(ERROR_MESSAGE)
-    }
+    var validPasswordInput = passwordInput !== "";
+    validUsernameInput && validPasswordInput ? location.href = 'app.html' : $("#SubmitButton").after(ERROR_MESSAGE);
 }
 
 $(document).ready(function() {
     $("#SubmitButton").click(function() {
         submit()
+    });
+
+    $(".form-control").click(function() {
+        clearErrorMessage();
     });
 });

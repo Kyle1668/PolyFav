f1b8b014ad3f67dd38064919b94d00be31fec3fb
Kyle1668 <kyledevinobrien1@gmail.com>
2017-07-22 19:44:12 -0700

    added error button

--- public/scripts/login/index.js
+++ public/scripts/login/index.js
@@ -2,6 +2,28 @@
  * Created by kyleobrien on 7/22/17.
  */
 
-$(document).ready(function() {
+var ERROR_MESSAGE = "<strong class=\"error\">Invalid Input: Username or password is incorrect.</strong>"
 
+function submit() {
+    var nameInput = $("#UserNameInput").val();
+    var passwordInput = $("#PasswordInput").val();
+    var errorMessagePrinted = $(".error").length === 0
+    var validInput = nameInput !== "" || passwordInput !== "";
+
+    console.log(validInput);
+    console.log(nameInput);
+    console.log(passwordInput);
+
+    if (validInput) {
+        /*location.href = 'app.html';*/
+    }
+    else if (errorMessagePrinted) {
+        $("#SubmitButton").after(ERROR_MESSAGE)
+    }
+}
+
+$(document).ready(function() {
+    $("#SubmitButton").click(function() {
+        submit()
+    });
 });

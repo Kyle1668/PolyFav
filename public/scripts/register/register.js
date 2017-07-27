e66e602d8c2f371a0b2f94b4db32c1c1543f576b
Kyle1668 <kyledevinobrien1@gmail.com>
2017-07-27 14:13:25 -0700

    finish reg page

--- public/scripts/register/register.js
+++ public/scripts/register/register.js
@@ -1,4 +1,22 @@
 
+function checkFields() {
+    return true
+}
+
 $(document).ready(function() {
+
+    $("#home").click(function() {
+        location.href = 'index.html'
+    });
+
+    $("#SubmitButton").click(function() {
+        if (checkFields()) {
+            // Database Stuff
+            location.href = 'index.html'
+        }
+        else {
+            // Print Error
+        }
+    });
 
 });

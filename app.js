7e934fa726f1c66568b3cf060ee5700c264e0ebe
Kyle1668 <kyledevinobrien1@gmail.com>
2017-08-16 11:08:23 -0700

    prog

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-
 var express = require("express");
-var app = express();
 var io = require("socket.io")();
+var app = express();
 
 var port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
+
 app.listen(port);
+
+app.get("/api", function (req, res) {
+    res.json({
+        Name: "Kyle",
+        Age: 20
+    });
+});
 
 console.log("app.js running");
